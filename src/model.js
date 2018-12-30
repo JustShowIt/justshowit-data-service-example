a8d8e4d5408c2b98cc8359bcc98eb79625f19d45
Mario Linz <gelight@gmx.de>
2018-12-30 17:55:30 +0100

    Update mock json.

--- src/model.js
+++ src/model.js
@@ -15,25 +15,31 @@ const mock = [
                 "units": [
                     {
                         "id": "4",
-                        "type": "TextField"
+                        "type": "TextField",
+                        "options": {
+                            "titel": "Neuronale Netze sind irgendwie total krass."
+                        }
                     }
                 ]
             },
             {
                 "id": "5",
-                "type": "Text"
+                "type": "Text",
+                "options": {
+                    "text": "Die sist ein kleiner Text."
+                }
             },
             {
                 "id": "6",
                 "options": {
-                "titel": "Lustiges Video",
-                "description": "Ein cooles lustiges Video mit einem Hasen.",
-                "path": "https://www.w3schools.com/html/mov_bbb.mp4",
-                "resolution": "320x240",
-                "runtime": "10.26s",
-                "size": "131.509.108",
-                "author": "Mario Linz",
-                "intelligentSearchDepth": 5
+                    "titel": "Lustiges Video",
+                    "description": "Ein cooles lustiges Video mit einem Hasen.",
+                    "path": "https://www.w3schools.com/html/mov_bbb.mp4",
+                    "resolution": "320x240",
+                    "runtime": "10.26s",
+                    "size": "131.509.108",
+                    "author": "Mario Linz",
+                    "intelligentSearchDepth": 5
                 }
             }
         ]
